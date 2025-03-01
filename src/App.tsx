3d1c2f6d0a14b6e70bdfe624f4f08ae696391610
henrqq <caiohenrqq@gmail.com>
2025-03-01 18:56:46 -0400

    Altera nome de App.css para global.css e adiciona Fonte Inter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import React from 'react'
 
 function App() {
   return (
     <>
-
+      <div className='titulo-inicio font-sans bg-red-500'>
+        AG Sistemas - Desafio Técnico
+      </div>
     </>
   )
 }
