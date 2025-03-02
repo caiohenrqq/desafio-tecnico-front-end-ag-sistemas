7b70d60ffab7a5c985f3814785d427b9d3987ac7
henrqq <caiohenrqq@gmail.com>
2025-03-02 02:00:56 -0400

    Adiciona botão que adiciona blur, e previne bugs causados pelo blur, como borda branca.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 import "./global.css";
+import { Consulta } from "./Consulta";
 
 function App() {
-  const [janelaAtiva, setJanelaAtiva] = useState<boolean>(false);
+  const [consultaAtiva, setConsultaAtiva] = useState<boolean>(false);
   
-  const abrirJanela = () => {
-    setJanelaAtiva(true);
+  const abrirConsulta = () => {
+    setConsultaAtiva(true);
   };
 
-  const fecharJanela = () => {
-    setJanelaAtiva(false);
+  const fecharConsulta = () => {
+    setConsultaAtiva(false);
   };
-
+  
   return (
     <>
-      <section className="inicio min-h-screen flex flex-col items-center gap-7 justify-center font-sans bg-primaria text-secundaria">
+      <section className={`inicio min-h-screen flex flex-col items-center gap-7 justify-center font-sans bg-primaria text-secundaria ${consultaAtiva ? "blur-sm" : "blur-none"}`}>
         <div className="titulo-inicio font-bold text-5xl">
           AG Sistemas - Desafio Técnico
         </div>
         {/* Botão para abrir popup */}
         <button
+          onClick={abrirConsulta}
           type="button"
           className="text-primaria hover:bg-gray-400 bg-secundaria rounded-full font-bold text-sm px-5 py-2.5 text-center inline-flex items-center"
         >
           Vamos lá!
+          
           {/* Flecha -> */}
           <svg
             className="rtl:rotate-180 w-6 h-6 ms-2"
@@ -41,6 +44,10 @@ function App() {
             />
           </svg>
         </button>
+        <div>
+          <Consulta/> 
+        </div>
+
       </section>
     </>
   );
---
+++ src/Consulta.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./global.css";
+
+export const Consulta = () => {
+  return (
+    <div className="">
+      <div className="w-32 h-32  items-center justify-center rounded-lg shadow-md bg-secundaria">
+        CEP
+      </div>
+    </div>
+  );
+};
