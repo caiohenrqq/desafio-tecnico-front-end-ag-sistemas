8cc2a9095c8cfbd9ca337290b58068d341a23f77
henrqq <caiohenrqq@gmail.com>
2025-03-02 01:18:06 -0400

    Adiciona cores no background e no texto, tambem adiciona botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
-import React from 'react'
-import './global.css'
+import { useState } from "react";
+import "./global.css";
 
 function App() {
+  const [janelaAtiva, setJanelaAtiva] = useState<boolean>(false);
+  
+  const abrirJanela = () => {
+    setJanelaAtiva(true);
+  };
+
+  const fecharJanela = () => {
+    setJanelaAtiva(false);
+  };
+
   return (
     <>
-    <section className='inicio'>
-      <div className='titulo-inicio min-h-screen flex items-center justify-center font-sans bg-primaria text-secundaria font-bold'>
-        AG Sistemas - Desafio Técnico
-      </div>
-    </section>
+      <section className="inicio min-h-screen flex flex-col items-center gap-7 justify-center font-sans bg-primaria text-secundaria">
+        <div className="titulo-inicio font-bold text-5xl">
+          AG Sistemas - Desafio Técnico
+        </div>
+        {/* Botão para abrir popup */}
+        <button
+          type="button"
+          className="text-primaria hover:bg-gray-400 bg-secundaria rounded-full font-bold text-sm px-5 py-2.5 text-center inline-flex items-center"
+        >
+          Vamos lá!
+          {/* Flecha -> */}
+          <svg
+            className="rtl:rotate-180 w-6 h-6 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </button>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
